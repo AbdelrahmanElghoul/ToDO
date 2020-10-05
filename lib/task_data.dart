e598d33f4b82d3e565cd9d79b74804f48b1bde83
AbdelrahmanElghoul <aelghoul0@gmail.com>
2020-10-05 22:26:19 +0200

    .

--- lib/task_data.dart
+++ lib/task_data.dart
@@ -2,9 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'Task.dart';
 
 class TaskData extends ChangeNotifier{
-  List<Task> taskList=[
-    Task(name: '1')
-  ];
+  List<Task> taskList=[];
 
   void addTask(String name){
     taskList.add(Task(name: name));
